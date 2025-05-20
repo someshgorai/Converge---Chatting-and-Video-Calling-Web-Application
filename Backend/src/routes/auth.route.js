547ed94eadc09530c2f165292085c62606fa1897
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-05-20 22:50:04 +0530

    Add onboarding and me endpoints

--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,10 +1,23 @@
 import express from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  onboard,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
-const router = express.Router(); //Chatgpt
+const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.post("/logout", logout);
+router.post("/logout", logout); // post changes the server state
+
+router.post("/onboarding", protectRoute, onboard);
+
+// Checks if user is logged in
+router.get("/me", protectRoute, (req, res) => {
+  res.status(200).json({ success: true, user: req.user });
+});
 
 export default router;
