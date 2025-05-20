c325b8452ca04f73df4c06eece629783b6dad241
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-05-20 22:55:45 +0530

    Add reference for upsertUsers

--- Backend/src/lib/stream.js
+++ Backend/src/lib/stream.js
@@ -12,7 +12,7 @@ const streamClient = StreamChat.getInstance(apiKey, apiSecret);
 
 export const upsertStreamUser = async (userData) => {
   try {
-    await streamClient.upsertUsers([userData]);
+    await streamClient.upsertUsers([userData]); // Refer: https://getstream.io/chat/docs/javascript/update_users/
     return userData;
   } catch (error) {
     console.error("Error upserting Stream user:", error);
