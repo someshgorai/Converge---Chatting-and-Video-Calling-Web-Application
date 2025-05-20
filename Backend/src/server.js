d25f2f14c61f566108ffb01dff5cfd1b6e9104d5
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-05-20 22:54:30 +0530

    Add cookieParser middleware

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import { db } from "./lib/sql/db.postgres.js";
@@ -9,6 +10,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json()); // Parses incoming requests with JSON payloads and puts the parsed data in req.body
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
