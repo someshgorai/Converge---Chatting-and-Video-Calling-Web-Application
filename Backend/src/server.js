8f42923e639268d2ace6b2f443267176990b3468
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-05-21 15:55:55 +0530

    Add chat endpoint

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
+import chatRoutes from "./routes/chat.route.js";
+
 import { connectDB } from "./lib/mongoDB/db.mongo.js";
 
 const app = express();
@@ -14,6 +16,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/chat", chatRoutes);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
