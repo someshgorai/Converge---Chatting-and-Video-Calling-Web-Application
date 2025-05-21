16a36ff307476c1f5262fc0c42c1114c5c2aaa5f
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-05-21 15:33:53 +0530

    Add acceptFriendRequest, getFriendRequests, getOutgoingFriendReqs endpoints

--- Backend/src/routes/user.route.js
+++ Backend/src/routes/user.route.js
@@ -4,6 +4,9 @@ import {
   getRecommendedUsers,
   getMyFriends,
   sendFriendRequest,
+  acceptFriendRequest,
+  getFriendRequests,
+  getOutgoingFriendReqs,
 } from "../controllers/user.controller.js";
 
 const router = express.Router();
@@ -15,5 +18,9 @@ router.get("/", getRecommendedUsers);
 router.get("/friends", getMyFriends);
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
+
+router.get("/friend-request", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router;
