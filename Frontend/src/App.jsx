ce638342df02fcd5e65aa2af6d2c3dfc7f84da1a
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-05-22 23:22:28 +0530

    Add react-query

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from "react-router";
 import { Toaster } from "react-hot-toast";
+import { useQuery } from "@tanstack/react-query";
 
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
@@ -8,16 +9,29 @@ import NotificationPage from "./pages/NotificationPage.jsx";
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
+import { axiosInstance } from "./lib/axios.js";
 
 
 const App = () => {
+  // tanstack query
+  const { data, isLoading, error } = useQuery({queryKey: ["todos"],
+
+    queryFn: async() => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+    },
+    retry: false, // auth check
+  })
+
+  console.log({data});
+
   return (
     <div className=" h-screen" data-theme="dark">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifiation" element={<NotificationPage />} />
+        <Route path="/notification" element={<NotificationPage />} />
         <Route path="/call" element={<CallPage />} />
         <Route path="/chat" element={<ChatPage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
