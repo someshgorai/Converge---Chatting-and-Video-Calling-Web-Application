60253771abfc71de5d9ca3b881373be44fba2d16
SOMESH GORAI <mailme.someshgorai@gmail.com>
2025-05-26 02:31:59 +0530

    Deployment and Final Tests

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -11,6 +12,8 @@ import { connectDB } from "./lib/mongoDB/db.mongo.js";
 
 const app = express();
 const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
 
 app.use(
   cors({
@@ -24,6 +27,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/chat", chatRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api";
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
-    withCredentials: true // send cookies with the request
-})
+  baseURL: BASE_URL,
+  withCredentials: true, // send cookies with the request
+});
